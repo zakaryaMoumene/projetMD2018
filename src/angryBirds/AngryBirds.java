a956ba4122b253e71f1ac72ebb7cb7bc44aff26d
Zakarya Moumene <zakarya.moumene@gmail.com>
2018-01-31 23:07:03 +0100

    ajout diagramme

--- src/angryBirds/AngryBirds.java
+++ src/angryBirds/AngryBirds.java
@@ -8,15 +8,17 @@ import java.awt.event.WindowEvent;
 // classe principale qui va permettre de lancer le jeu
 public class AngryBirds {
 
+    static Window window;
+    
     public static void main(String[] args) {
         Frame frame = new Frame("Oiseau pas content");
-        final Window obj = new Window();
+        window = new Window();
         frame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent event) {
                 System.exit(0);
             }
         });
-        frame.add(obj);
+        frame.add(window);
         frame.pack();
         frame.setVisible(true);
 
--- src/angryBirds/ArenaComponentBuilder.java
+++ src/angryBirds/ArenaComponentBuilder.java
@@ -2,12 +2,12 @@ package angryBirds;
 
 // pour fournir les objet allant composer l'arène  
 public class ArenaComponentBuilder {
-
-    public Gravity createGravity(double d){
+   
+    public Gravity createGravity(double d) {
         return new Gravity(d);
     }
-    
-    public CollisionEngine createCollisionEngine(){
+
+    public CollisionEngine createCollisionEngine() {
         return new CollisionEngine();
     }
 }
